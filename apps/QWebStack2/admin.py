a5b077ce13bc1a737ad5ee67c6592fa1b691547e
龙宇 <longyu_w@foxmail.com>
2023-05-24 03:07:19 +0800

    添加图片404重定向

--- apps/QWebStack2/admin.py
+++ apps/QWebStack2/admin.py
@@ -11,19 +11,27 @@ class TitlesAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['pk', 'id', 'name', 'icon', ]
+    list_display_links = ['name']
+
 
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ['pk', 'id', 'name', 'parent', ]
+    list_display_links = ['name']
 
 
 @admin.register(Site)
 class SiteAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'id', 'url', 'logo_url', 'name', 'describtion', 'is_show', 'category', ]
+    list_display = ['id', 'name', 'url', 'logo_url','is_show', 'category', ]
+    list_display_links = ['url','name']
+    list_filter = ('category',)
+    search_fields = ['id', 'url', 'logo_url', 'name','is_show', 'category', ]
 
 
 @admin.register(friendlink)
 class friendlinkAdmin(admin.ModelAdmin):
     list_display = ['pk', 'id', 'name', 'url', 'is_show', ]
+    list_display_links = ['url','name']
+
 
